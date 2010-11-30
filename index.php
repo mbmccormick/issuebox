927f76b28e7ca7b4297e63f16d844c47e6772cf7
Matt McCormick <mbmccormick@gmail.com>
2010-11-30 15:51:10 -0500

    Updated index page.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
     require "config.php";
+    require "utils.php";
     require_once "security.php";
     
     authorize();
@@ -17,63 +18,153 @@
 <?php include "header.php"; ?>
     <div class="content">
         <div class="navigation">
-            <a href="index.php">Projects</a>
+            <a href="index.php">Home</a>
         </div>
         <div class="list">
             <?php
 
-                $result = mysql_query("SELECT * FROM project ORDER BY name ASC");
+                $result = mysql_query("SELECT * FROM activity ORDER BY createddate DESC LIMIT 20");
                 while($row = mysql_fetch_array($result))
                 {
-                    $sql = mysql_query("SELECT COUNT(*) AS rowcount FROM issue WHERE projectid = '$row[id]' AND isclosed = '0'");
-                    $return = mysql_fetch_array($sql);
-                    $open = $return[rowcount];
+                    $sql = mysql_query("SELECT * FROM user WHERE id = '$row[createdby]'");
+                    $user = mysql_fetch_array($sql);
                     
-                    $sql = mysql_query("SELECT COUNT(*) AS rowcount FROM issue WHERE projectid = '$row[id]' AND isclosed = '1'");
-                    $return = mysql_fetch_array($sql);
-                    $closed = $return[rowcount];
-                                        
-                    echo "<div class='list-item project'>\n";
-                    echo "<table cellpadding='0' cellspacing='0' style='width: 100%;'><tr>\n";
+                    echo "<div class='list-item activity'>\n";
+                    echo "<table cellpadding='0' cellspacing='0' style='width: 100%;'>\n";
                     
-                    echo "<td width='100%'>\n";
-                    echo "<h3><a href='project.php?id=$row[id]'>" . $row[name] . "</a></h3><br />\n";
-                    echo "<p>" . $row[description] . "</p>\n";
-                    echo "</td>\n";
+                    if ($row[itemtype] == "1")
+                    {
+                        $sql = mysql_query("SELECT * FROM project WHERE id = '$row[itemid]'");
+                        $project = mysql_fetch_array($sql);
+                        
+                        echo "<tr>\n";
+                        echo "<td rowspan='2' valign='top' style='width: 25px;'>\n";
+                        echo "<img src='img/project" . $row[actiontype] . ".png' alt='$row[actiontype]' />\n";
+                        echo "</td>\n";
+                        echo "<td colspan='2' style='padding-bottom: 7px;'>\n";
+                        echo "<b><a href='user-edit.php?id=" . $user[id] . "'>" . $user[username] . "</a> ";
+                        
+                        if ($row[actiontype] == "1")
+                        {
+                            echo "created ";
+                        }
+                        else if ($row[actiontype] == "2")
+                        {
+                            echo "updated ";
+                        }
+                        else if ($row[actiontype] == "3")
+                        {
+                            echo "deleted ";
+                        }
+                        
+                        echo "project <a href='project-edit.php?id=$project[id]'>" . $project[name] . "</a> about " . FriendlyDate(1, strtotime($row[createddate]));
+                        echo "</b>\n";
+                        echo "</td>\n";
+                        echo "</tr>\n";
+                        echo "<tr>\n";
+                        echo "<td valign='top' style='width: 45px;'>\n";
+                        echo "<img src='http://www.gravatar.com/avatar/" . md5($user[email]) . "?s=30' style='padding: 2px; border: 1px solid #D0D0D0; background-color: #ffffff;' />\n";
+                        echo "</td>\n";
+                        echo "<td valign='top'>\n";
+                        echo "<span class='description'>" . FriendlyString($project[description]) . "</span>\n";
+                        echo "</td>\n";
+                        echo "</tr>\n";
+                    }
+                    else if ($row[itemtype] == "2")
+                    {
+                        $sql = mysql_query("SELECT * FROM issue WHERE id = '$row[itemid]'");
+                        $issue = mysql_fetch_array($sql);
+                        
+                        $sql = mysql_query("SELECT * FROM project WHERE id = '$issue[projectid]'");
+                        $project = mysql_fetch_array($sql);
+                        
+                        echo "<tr>\n";
+                        echo "<td rowspan='2' valign='top' style='width: 25px;'>\n";
+                        echo "<img src='img/issue" . $row[actiontype] . ".png' alt='$row[actiontype]' />\n";
+                        echo "</td>\n";
+                        echo "<td colspan='2' style='padding-bottom: 7px;'>\n";
+                        echo "<b><a href='user-edit.php?id=" . $user[id] . "'>" . $user[username] . "</a> ";
+                        
+                        if ($row[actiontype] == "1")
+                        {
+                            echo "created ";
+                        }
+                        else if ($row[actiontype] == "2")
+                        {
+                            echo "updated ";
+                        }
+                        else if ($row[actiontype] == "3")
+                        {
+                            echo "deleted ";
+                        }
+                                                
+                        echo "<a href='issue-edit.php?id=$issue[id]'>issue " . $issue[number] . "</a> on <a href='project-edit.php?id=$project[id]'>" . $project[name] . "</a> about " . FriendlyDate(1, strtotime($row[createddate]));
+                        echo "</b>\n";
+                        echo "</td>\n";
+                        echo "</tr>\n";
+                        echo "<tr>\n";
+                        echo "<td valign='top' style='width: 45px;'>\n";
+                        echo "<img src='http://www.gravatar.com/avatar/" . md5($user[email]) . "?s=30' style='padding: 2px; border: 1px solid #D0D0D0; background-color: #ffffff;' />\n";
+                        echo "</td>\n";
+                        echo "<td valign='top'>\n";
+                        echo "<span class='description'>" . FriendlyString($issue[body]) . "</span>\n";
+                        echo "</td>\n";
+                        echo "</tr>\n";
+                    }
+                    else if ($row[itemtype] == "3")
+                    {
+                        $sql = mysql_query("SELECT * FROM comment WHERE id = '$row[itemid]'");
+                        $comment = mysql_fetch_array($sql);
+                        
+                        $sql = mysql_query("SELECT * FROM issue WHERE id = '$comment[issueid]'");
+                        $issue = mysql_fetch_array($sql);
+                        
+                        echo "<tr>\n";
+                        echo "<td rowspan='2' valign='top' style='width: 25px;'>\n";
+                        echo "<img src='img/comment" . $row[actiontype] . ".png' alt='$row[actiontype]' />\n";
+                        echo "</td>\n";
+                        echo "<td colspan='2' style='padding-bottom: 7px;'>\n";
+                        echo "<b><a href='user-edit.php?id=" . $user[id] . "'>" . $user[username] . "</a> ";
+                        
+                        if ($row[actiontype] == "1")
+                        {
+                            echo "commented on ";
+                        }
+                        else if ($row[actiontype] == "2")
+                        {
+                            echo "updated a comment on ";
+                        }
+                        else if ($row[actiontype] == "3")
+                        {
+                            echo "deleted a comment on ";
+                        }
+                        
+                        echo "<a href='issue-edit.php?id=$issue[id]'>issue " . $issue[number] . "</a> about " . FriendlyDate(1, strtotime($row[createddate]));
+                        echo "</b>\n";
+                        echo "</td>\n";
+                        echo "</tr>\n";
+                        echo "<tr>\n";
+                        echo "<td valign='top' style='width: 45px;'>\n";
+                        echo "<img src='http://www.gravatar.com/avatar/" . md5($user[email]) . "?s=30' style='padding: 2px; border: 1px solid #D0D0D0; background-color: #ffffff;' />\n";
+                        echo "</td>\n";
+                        echo "<td valign='top'>\n";
+                        echo "<span class='description'>" . FriendlyString($comment[body]) . "</span>\n";
+                        echo "</td>\n";
+                        echo "</tr>\n";
+                    }
                     
-                    echo "<td>\n";
-                    echo "<div class='counter'>\n";
-                    echo "<big>$open</big>\n";
-                    echo "Open Issues\n";
-                    echo "</div>\n";
-                    echo "</td>\n";
-                    
-                    echo "<td>\n";
-                    echo "<div class='counter'>\n";
-                    echo "<big>$closed</big>\n";
-                    echo "Closed Issues\n";
-                    echo "</div>\n";
-                    echo "</td>\n";    
-                    
-                    echo "</tr></table>\n";
+                    echo "</table>\n";
                     echo "</div>\n";
                 }
                 
                 if (mysql_num_rows($result) == 0)
                 {
-                    echo "<div class='list-item project'>\n";
-                    echo "<p>There are currently no projects setup.</p>\n";
+                    echo "<div class='list-item activity'>\n";
+                    echo "<p>There is currently no activity to display.</p>\n";
                     echo "</div>\n";
                 }
 
             ?>
         </div>
-        <br />
-        <button type="button" class="button" onclick="location.href='project-add.php';">
-            <span>New Project</span>
-        </button>
-        <button type="button" class="button" onclick="location.href='settings.php';">
-            <span>Settings</span>
-        </button>
     </div>
 <?php include "footer.php"; ?>
