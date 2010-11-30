b9778c5977e7c98875725a3ba5d4518cc69005ef
Matt McCormick <mbmccormick@gmail.com>
2010-11-30 15:41:34 -0500

    Added limit parameter to activities.

--- activity.php
+++ activity.php
@@ -23,7 +23,7 @@
         <div class="list">
             <?php
 
-                $result = mysql_query("SELECT * FROM activity ORDER BY createddate DESC");
+                $result = mysql_query("SELECT * FROM activity ORDER BY createddate DESC LIMIT 20");
                 while($row = mysql_fetch_array($result))
                 {
                     $sql = mysql_query("SELECT * FROM user WHERE id = '$row[createdby]'");
