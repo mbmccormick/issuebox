d6d222923dce13cbe959a41caefbf598625489e5
Matt McCormick <mbmccormick@gmail.com>
2010-12-02 16:15:11 -0500

    Modified the login page.

--- login.php
+++ login.php
@@ -4,20 +4,10 @@
     require_once "security.php";
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml" dir=ltr lang=en-US> 
-<head> 
-  <title>Issue Tracker</title> 
-  <link rel="stylesheet" type="text/css" media="screen,projection" href="css/stylesheet.css" />
-  <script type="text/javascript" src="js/css_browser_selector/css_browser_selector.js"></script>
-  <script type="text/javascript" src="js/github-flavored-markdown/scripts/showdown.js"></script>
-</head> 
-<body> 
-    <div class="header" id="login-header">
-    </div>
+<?php include "header.php"; ?>
     <div class="content">
         <div id="login" class="standard_form">
-            <h2><a href="index.php">Issue Tracker</a></h2>
+            <h2>Login</h2>
             <br />
             <form action="login_post.php" method="post">
                 <label for="Username">
@@ -34,8 +24,4 @@
             </form>  
         </div>
     </div>
-    <div class="footer" id="login-footer">
-        Copyright &copy; 2010 McCormick Technologies LLC. All rights reserved.
-    </div>
-</body> 
-</html>
+<?php include "footer.php"; ?>
