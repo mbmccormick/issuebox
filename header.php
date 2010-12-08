1ed011b2f848c675fe65ebdcfb1146ed2c8dbef3
Matt McCormick <mbmccormick@gmail.com>
2010-12-08 12:56:12 -0500

    Updated order of import.

--- header.php
+++ header.php
@@ -8,12 +8,12 @@
 <head> 
   <title>Issue Tracker</title> 
   <link rel="stylesheet" type="text/css" media="screen,projection" href="css/stylesheet.css" />
-  <script type="text/javascript" src="js/common.js"></script>
-  <script type="text/javascript" src="js/css_browser_selector/css_browser_selector.js"></script>
-  <script type="text/javascript" src="js/github-flavored-markdown/scripts/showdown.js"></script>
   <script type="text/javascript" src="js/jquery/jquery-1.4.4.js"></script>
   <script type="text/javascript" src="js/form/jquery.form.js"></script>
   <script type="text/javascript" src="js/showMessage/jquery.showMessage-2.1.js"></script>
+  <script type="text/javascript" src="js/css_browser_selector/css_browser_selector.js"></script>
+  <script type="text/javascript" src="js/github-flavored-markdown/scripts/showdown.js"></script>
+  <script type="text/javascript" src="js/common.js"></script>
 </head> 
 <body> 
     <?php 
