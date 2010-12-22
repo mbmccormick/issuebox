83a705922f8a9ac73767c7984251e5e2c1ae7f16
Matt McCormick <mbmccormick@gmail.com>
2010-12-22 12:36:31 -0500

    Updated the database structure file.

--- db/database.sql
+++ db/database.sql
@@ -1,5 +1,17 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
+CREATE TABLE IF NOT EXISTS `activity` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `headline` varchar(500) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `itemid` int(11) DEFAULT NULL,
+  `itemtype` int(10) DEFAULT NULL,
+  `actiontype` int(10) DEFAULT NULL,
+  `createdby` int(10) DEFAULT NULL,
+  `createddate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=696 ;
+
 CREATE TABLE IF NOT EXISTS `comment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `issueid` int(11) NOT NULL,
@@ -7,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `createdby` int(11) NOT NULL,
   `createddate` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=149 ;
 
 CREATE TABLE IF NOT EXISTS `issue` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -16,10 +28,11 @@ CREATE TABLE IF NOT EXISTS `issue` (
   `title` varchar(500) NOT NULL,
   `body` varchar(5000) NOT NULL,
   `isclosed` tinyint(1) NOT NULL,
+  `isurgent` tinyint(1) NOT NULL,
   `createdby` int(11) NOT NULL,
   `createddate` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=74 ;
 
 CREATE TABLE IF NOT EXISTS `project` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -28,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `project` (
   `createdby` int(11) NOT NULL,
   `createddate` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -37,4 +50,4 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(100) NOT NULL,
   `createddate` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
